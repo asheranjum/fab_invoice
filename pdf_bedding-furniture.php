732a9b78fe23723351b470a750304e82cb71c586
Hassan <hassanimran1901@gmail.com>
2025-08-25 20:26:24 +0500

    code

--- pdf_bedding-furniture.php
+++ pdf_bedding-furniture.php
@@ -307,12 +307,12 @@ foreach ($groupedItems as $runsheetNumber => $runsheetData) {
        
         $html .= '
         <tr >
-            <td style="  text-align: left; width: 20.5%; padding:0px 4px;">Invoice# '.$customInvoiceNo .' <br> Account: ' . htmlspecialchars($customInvoiceName2). '</td>
+            <td style="  text-align: left; width: 20.5%; padding:0px 4px;">Account: '.htmlspecialchars($customInvoiceName2) .' <br> Invoice# ' .$customInvoiceNo. '</td>
             <td style="padding:0px; width: 73%; ">
                 <table class="checkbox-table">
                     <tr>';
 
-        $allOptions = $items; // Only use items fetched from DB
+                    $allOptions = $items; // Only use items fetched from DB
         // Check for any key matching P/UP(x)
         $selectedPUP = null; // To store the matched P/UP key
         foreach ($items as $key => $value) {
